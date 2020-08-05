bd67732218e9ee1c5645b8fd85c267ec7e031051
Alex Devero <deveroalex@gmail.com>
2020-08-05 13:03:05 +0200

    Add global context/state and new pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,38 +5,47 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Home } from './pages/home'
 import { About } from './pages/about'
 // import { Deals } from './pages/deals'
-import { Donate } from './pages/donate'
+// import { Donate } from './pages/donate'
 import { Metrics } from './pages/metrics'
 // import { Settings } from './pages/settings'
+import { SignIn } from './pages/sign-in'
+import { SignUp } from './pages/sign-up'
 import { Subscribe } from './pages/subscribe'
 
 import { Footer } from './components/footer'
 import { Header } from './components/header'
 
+import { GlobalContextProvider } from './state/state'
+
 import './styles/reset.css'
 import './styles/styles.css'
 import './styles/typography.css'
+import './styles/dark-mode.css'
 
 import * as serviceWorker from './serviceWorker'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container page-wrapper">
-        <Header />
+      <GlobalContextProvider>
+        <div className="container page-wrapper">
+          <Header />
 
-        <Switch>
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/about" component={About} />
-          {/* <Route path="/deals" component={Deals} /> */}
-          <Route path="/donate" component={Donate} />
-          <Route path="/metrics" component={Metrics} />
-          {/* <Route path="/settings" component={Settings} /> */}
-          <Route path="/subscribe" component={Subscribe} />
-        </Switch>
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+            {/* <Route path="/deals" component={Deals} /> */}
+            {/* <Route path="/donate" component={Donate} /> */}
+            <Route path="/metrics" component={Metrics} />
+            {/* <Route path="/settings" component={Settings} /> */}
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/subscribe" component={Subscribe} />
+          </Switch>
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
+      </GlobalContextProvider>
     </BrowserRouter>
   )
 }
@@ -47,4 +56,4 @@ render(<App />, rootElement)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.unregister()

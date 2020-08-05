77feeb6d1995fe546f052437f00fa592027bf244
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:49:35 +0200

    Add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const About = () => (
+  <div className="page page-about">
+    <div className="heading-wrapper">
+      <h1 className="mt-4 mb-4">About</h1>
+    </div>
+
+    <div className="row justify-content-center">
+      <div className="col-md-8 col-lg-6">
+        <p className="mb-4">Growing any business is difficult, but scaling a software as a service (SaaS) company is especially hard.</p>
+
+        <p className="mb-4">The problem most SaaS companies struggle with is achieving predictable revenue growth. Even public SaaS companies struggle to achieve profitability.</p>
+
+        <p className="mb-4">To make a SaaS company successful, you can’t just change your software delivery model to the web and expect it all to work. You must make good, data-driven decisions when it comes to your marketing, sales, and customer success operations.</p>
+
+        <p className="mb-4">To make data-driven decisions, you have to track the right SaaS metrics and KPIs. The goal of this app is to provide you with these metrics.</p>
+
+        <p>If you think some metric is missing, and should be on the list, shoot me an <a href="mailto:deveroalex@gmail.com">e-mail</a>.</p>
+      </div>
+    </div>
+  </div>
+)

3a9ef92241213da434553269bf41cc3e32ac5c91
Alex Devero <deveroalex@gmail.com>
2020-08-05 13:01:11 +0200

    Finish implementation of global state

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -1,14 +1,28 @@
 import React, { createContext, useState } from 'react'
 
-const UsertContext = createContext()
-
-export const UserProvider = ({ children }: any ) => {
-  const [darkMode, setDarkMode] = useState(false)
-
-  return (
-    <UsertContext.Provider value={{darkMode, setDarkMode}}>
-      {children}
-    </UsertContext.Provider>
-  )
+interface GlobalContextUI {
+  darkMode: string;
+  handleDarkMode: () => void;
 }
 
+interface GlobalContextProviderUI {
+  children: React.ReactChildren
+}
+
+export const GlobalContext = createContext<GlobalContextUI>({})
+
+export const GlobalContextProvider = (props: GlobalContextProviderUI) => {
+  const [darkMode, setDarkMode] = useState('off')
+
+  const handleDarkMode = () => {
+    setDarkMode(darkMode === 'off' ? 'on' : 'off')
+
+    document.body.classList.toggle('dark-mode')
+  }
+
+  return (
+    <GlobalContext.Provider value={{ darkMode, handleDarkMode }}>
+      {props.children}
+    </GlobalContext.Provider>
+  )
+}

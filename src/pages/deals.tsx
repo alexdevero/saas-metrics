6a5f51c57a8a187cd2ffa10ed2f08fd275aeb911
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:49:44 +0200

    Add deals page

---
+++ src/pages/deals.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Deals = () => (
+  <div className="page page-deals">
+    <div className="heading-wrapper">
+      <h1 className="mt-4 mb-4">Deals</h1>
+    </div>
+  </div>
+)

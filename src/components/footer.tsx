28a7681cf215a7aed52751eb3973e7694c5d588d
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:42:36 +0200

    Add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './../styles/footer.css'
+
+export const Footer = () => (
+  <footer>
+    <p>Copyright &copy; {new Date().getFullYear()} Alex Devero</p>
+  </footer>
+)

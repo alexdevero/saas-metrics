fa21724124b77e205fc5ebeb36b89e4064dfcbd8
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:50:26 +0200

    Add subscribe page

---
+++ src/pages/subscribe.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+export const Subscribe = () => {
+  const [email, setEmail] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccesMessage] = useState('')
+
+  const submitForm = () => {
+    if (email.length > 0 && /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+      console.log(email)
+      setErrorMessage('')
+
+      axios
+        .post('url to php file', email)
+        .then(res => {
+          setEmail('')
+          setSuccesMessage(res.data)
+        })
+        .catch(err => setErrorMessage(err))
+    } else {
+      setErrorMessage('Please use correct email address.')
+    }
+  }
+
+  return (
+    <div className="page page-deals">
+      <div className="heading-wrapper">
+        <h1 className="mt-4 mb-4">Subscribe</h1>
+      </div>
+
+      <div className="row justify-content-center flex-column align-items-center">
+        <div className="col-md-8 col-lg-7 text-center">
+          <p className="mb-4">Stay updated about new metrics and also new SaaS projects you might be interested in.</p>
+        </div>
+
+        <div className="col-sm-8 col-md-6 col-lg-4 text-center">
+          <div className="form-group form-inline">
+            <input
+              type="text"
+              placeholder="Enter your email..."
+              className="form-control rounded-left flex-sm-grow-1"
+              value={email}
+              onChange={(event) => setEmail(event.currentTarget.value)}
+            />
+
+            <button onClick={submitForm} className="btn btn-primary flex-grow-1 flex-sm-grow-0 mt-2 mt-sm-0">Sign up</button>
+          </div>
+
+          {successMessage.length > 0 && (
+            <div className="text-danger font-weight-bold small">{errorMessage}</div>
+          )}
+
+          {errorMessage.length > 0 && (
+            <div className="text-danger font-weight-bold small">{errorMessage}</div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}

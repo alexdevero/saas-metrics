5017f78986b803001e9646f05aed9cdd805b17ba
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:50:11 +0200

    Add settings page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Settings = () => (
+  <div className="page page-settings">
+    <div className="heading-wrapper">
+      <h1 className="mt-4 mb-4">Settings</h1>
+    </div>
+  </div>
+)

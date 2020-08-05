adfbc8acbb2728e53af8c56f59c5282ac758409f
Alex Devero <deveroalex@gmail.com>
2020-08-05 12:43:02 +0200

    Add metrics data

---
+++ src/data/metrics.ts
@@ -0,0 +1,172 @@
+/*
+merics:
+- https://databox.com/metrics-every-saas-company-should-track
+- https://blog.hubspot.com/service/saas-metrics
+- https://www.geckoboard.com/best-practice/kpi-examples/#marketing-kpis
+- https://corporatefinanceinstitute.com/resources/knowledge/
+*/
+
+export const metrics = [
+{
+  id: 'saasm_0001',
+    name: 'Average First Response Time',
+    description: 'Average first response time is the average amount of time it takes for customer support to respond to a case after it’s submitted by a requester (customer).',
+    importance: ''
+  },
+  {
+    id: 'saasm_0002',
+    name: 'Average Resolution Time',
+    description: 'Average resolution time is the average amount of time it takes your support team to completely resolve or close a ticket. Average resolution time is a stronger corollary to customer satisfaction and retention than first response time.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0003',
+    name: 'Average Revenue Per Account (ARPA)',
+    description: 'Average revenue per account (ARPA), also known as average revenue per user/unit (ARPU), is a measure of the revenue generated per account (usually per month as most subscription businesses operate monthly). But you can always calculate it yearly or quarterly according to your plans and billing options.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0004',
+    name: 'CAC: LTV Ratio',
+    description: 'CLV-to-CAC shows the lifetime value of your customers and the total amount you spend to acquire them -- in a single metric. This metric displays the health of your marketing program, so you can invest in programs that are working well or change campaigns that aren\'t.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0005',
+    name: 'Conversion Rate to Customer',
+    description: '',
+    importance: ''
+  },
+  {
+    id: 'saasm_0006',
+    name: 'Customer Acquisition Cost (CAC)',
+    description: 'Customer acquisition cost (CAC) shows exactly how much it costs to acquire new customers and how much value they bring to your business. When combined with CLV, this metric helps companies guarantee that their business model is viable.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0007',
+    name: 'Customer Engagement Score',
+    description: 'A customer engagement score can provide you with a glimpse at how engaged a customer is -- how often they\'re logging in, what they\'re using your software for, and other contributing metrics that indicate the likelihood they will or will not churn.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0008',
+    name: 'Customer Health Score',
+    description: 'Customer Health Scoring assigns different values to different signals of customer loyalty or customer churn. It helps your customer-facing employees get a bird\'s-eye view of how their portfolio of customers are doing so they can reach out to customers at risk of churning with educational resources, additional support, before they actually lose them.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0009',
+    name: 'Customer Lifetime Value (LTV)',
+    description: 'Customer lifetime value (CLV) is the average amount of money that your customers pay during their engagement with your company. The metric provides businesses with an accurate portrayal of their growth.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0010',
+    name: 'Customer Retention Rate',
+    description: 'Customer Retention Rate can be defined as a metric that indicates the proportion of customers that have continued to use your product for a while. The opposite of retention is churn—also known as customer attrition.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0011',
+    name: 'Churn Rate',
+    description: 'Churn rate is the proportion of customers or subscribers who leave during a given time period. It is often an indicator for customer dissatisfaction, cheaper and/or better offers from the competition, aggressive and successful marketing from the competition, or reasons outside of your control like business failure or strategy shifts.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0012',
+    name: 'Lead-to-Customer Rate',
+    description: 'Lead-to-Customer Rate shows exactly how well you\'re generating sales-ready leads -- and improving over time. It outlines, on average, how many leads turn into paying customers, i.e. shows whether your sales process and lead-nurturing methods are working or not.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0013',
+    name: 'Leads by Lifecycle Stage',
+    description: '',
+    importance: ''
+  },
+  {
+    id: 'saasm_0014',
+    name: 'Monthly Recurring Revenue (MRR)',
+    description: 'To calculate Monthly Recurring Revenue summarize all the revenue you receive from your paying customers per month. Another way is to multiply the total number of paying customers by the average revenue per user (ARPU).',
+    importance: ''
+  },
+  {
+    id: 'saasm_0015',
+    name: 'Monthly Unique Visitors',
+    description: 'Monthly unique visitors is a count of the number of unique individuals who visited your website in a given month. If someone visits your site multiple times, he/she will only be counted as one unique visitor (assuming the same device and browser is used for each visit and the visitor doesn’t clear his/her cookies between visits).',
+    importance: ''
+  },
+  {
+    id: 'saasm_0016',
+    name: 'Months to Recover CAC',
+    description: 'Months to Recover CAC helps determine how long after you\'ve closed a customer you recoup the total CAC. It gives you an idea of how quickly a customer starts to generate ROI for your business -- and you want that number to get smaller over time as your business grows.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0017',
+    name: 'Net Promoter Score (NPS)',
+    description: 'Net Promoter Score (NPS) is the most popular metric to measure customer satisfaction and loyalty.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0018',
+    name: 'Number of Active Users',
+    description: 'Active users means the number of people that are actively using your product. This metric is a benchmark to determine the health of a SaaS company’s customer base. More usage by more users is a strong sign of a healthy SaaS app.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0019',
+    name: 'Number of Support Tickets Created',
+    description: 'The number of support tickets created is a measure of how many customers are requesting help. ',
+    importance: ''
+  },
+  {
+    id: 'saasm_0020',
+    name: 'Organic vs. Paid Traffic ROI',
+    description: 'Organic traffic metrics include visitors who arrive from a non-paid (organic) listing in the search results. Paid traffic metrics, on the other hand, include visitors who arrived from paid search results like pay-per-click (PPC) ads.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0021',
+    name: 'Product-Qualified Leads (PQLs)',
+    description: 'Product-Qualified Leads, or (PQLs), are “potential customers who have used a product and reached pre-defined triggers that signify a strong likelihood to become a paying customer.”',
+    importance: ''
+  },
+  {
+    id: 'saasm_0022',
+    name: 'Qualified Lead Velocity Rate (LVR)',
+    description: '',
+    importance: ''
+  },
+  {
+    id: 'saasm_0023',
+    name: 'Qualified Marketing Traffic',
+    description: '',
+    importance: ''
+  },
+  {
+    id: 'saasm_0024',
+    name: 'Revenue Run Rate',
+    description: 'Revenue Run Rate is an indicator of financial performance that takes a company’s current revenue in a certain period (a week, month, quarter, etc.) and converts it to an annual figure get the full-year equivalent.  This metric is often used by rapidly growing companies, as data that’s even a few months old can understate the current size of the company. Another term for this is the Sales Run Rate.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0025',
+    name: 'Signup to Subscriber Conversion Rate',
+    description: 'Signup to Subscriber Conversion Rate is the percentage of people who signup for a freemium or trial version of your product and convert to become a paying customer.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0026',
+    name: 'Viral Coefficient',
+    description: 'Viral Coefficient is the number of new users an existing user generates. This metric calculates the exponential referral cycle - sometimes called virality - that accelerates company growth. Virality is the inherent incentive for customers to refer friends or colleagues to your company.',
+    importance: ''
+  },
+  {
+    id: 'saasm_0027',
+    name: 'Lead Velocity Rate (LVR)',
+    description: 'Lead velocity rate (LVR) is a measure of the growth percentage change of qualified leads from month to month. It measures the efficiency and effectiveness of the company’s sales pipeline. LVR is one of the most critical key performance indicators (KPIs) of SaaS companies. Generally, companies use lead velocity rate to assess the efficiency of its sales, as well as its growth potential.',
+    importance: ''
+  }
+]

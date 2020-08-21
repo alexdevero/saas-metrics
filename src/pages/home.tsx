f500b9ff29ab69f3e5da716530e5e5c4aed5fb5e
Alex Devero <deveroalex@gmail.com>
2020-08-21 16:16:38 +0200

    Fix homepage grid

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,17 +9,23 @@ export const Home = () => (
     </Helmet>
 
     <div className="page page-home">
-      <div className="heading-wrapper mb-4">
-        <h1 className="h1 mt-0">SaaS Metrics <span aria-label="a rocket blasting off" role="img">🚀</span></h1>
-      </div>
+      <div className="row justify-content-center">
+        <div className="col-sm-10 col-md-8">
+          <div className="heading-wrapper mb-4">
+            <h1 className="h1 mt-0">SaaS Metrics <span aria-label="a rocket blasting off" role="img">🚀</span></h1>
+          </div>
 
-      <div className="row justify-content-center mb-4">
-        <div className="col-lg-10 text-center">
-          <p>The most important metrics and KPIs every SaaS company should track on one place.</p>
+          <div className="row justify-content-center mb-4">
+            <div className="col-lg-10 text-center">
+              <p>The most important metrics and KPIs every SaaS company should track on one place.</p>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-center">
+            <Link to="/metrics" className="btn btn-primary btn-round">See metrics</Link>
+          </div>
         </div>
       </div>
-
-      <Link to="/metrics" className="btn btn-primary btn-round">See metrics</Link>
     </div>
   </>
 )
